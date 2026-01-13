e60a0280b3e03c538b8314a12594aeadd14afbca
arham-ali1323 <arham.ali1323@gmail.com>
2026-01-14 00:57:08 +0500

    update tranier

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import logo from "../../public/images/german fitness.png";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function GymFooter() {
@@ -29,7 +29,13 @@ export default function GymFooter() {
             <div className="flex items-center gap-3">
               <div className="w-50 h-50 relative">
                 <Link href="/">
-                  <img src={logo.src} alt="footer-logo" className="w-full h-full object-contain"/>
+                  <Image 
+                  src="/images/german fitness.png" 
+                  alt="footer-logo" 
+                  width={200}
+                  height={200}
+                  className="w-full h-full object-contain"
+                />
                 </Link>
               </div>
             </div>
